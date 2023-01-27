5c19c5ab89021034f0f21ca2d62d497095069b66
Marcelo Mantilla <marcelo.mantilla@appcabinet.com>
2023-01-27 17:32:56 -0500

    define routes & associated layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'students#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :students, only: [:new, :index, :create]
 end
